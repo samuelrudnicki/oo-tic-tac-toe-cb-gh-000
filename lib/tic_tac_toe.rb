f5ef4954e1161a1ff924cc863abe87ea024563f6
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-14 22:17:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,7 +90,7 @@ class TicTacToe
       turn
     end
     if winner
-      puts("Congratulations #{winner(board)}!")
+      puts("Congratulations #{winner}!")
     else
       puts("Cat's Game!")
     end
