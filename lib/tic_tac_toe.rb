b75983153c77328bbed7211d5748ed1475824c30
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-14 22:08:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,13 +47,12 @@ class TicTacToe
     turn_count % 2 == 0 ? "X" : "O"
   end
 
-  def turn
-    def turn(board)
+    def turn
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board,index)
-    move(board, index, current_player(board))
+  if valid_move?(index)
+    move(index, current_player(board))
     display_board(board)
   else
     turn(board)
