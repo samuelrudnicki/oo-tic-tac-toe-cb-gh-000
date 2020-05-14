480a3680d5507f54d5c58171aaf33e71bb3632b6
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-14 21:56:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,4 +34,8 @@ class TicTacToe
   def position_taken?(index)
     @board[index] != "" && @board[index] != " " && @board[index] != nil
   end
+
+  def valid_move?(index)
+    !position_taken?(index) && index.between?(0,8)
+  end
 end
